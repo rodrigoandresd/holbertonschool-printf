1982600ecb3251306d7915c1ad9d76b290453741
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-18 16:35:55 -0500

    2do

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - Function that produces output according to a format
+ *
+ *
+ *
+ */
+
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/* importing */
+#include <stdio.h>
+
+/* types and structures */
+
+/* macros valores constantes */
+
+/* prototypes */
+
+int _printf(const char *format, ...);
+
+/* macros functions */
+
+#endif /* MAIN_H */
