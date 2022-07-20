657ee2f3521e45d32cdc4678048c6ad2d05163b3
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-19 22:51:59 -0500

    continue fun

--- _printf.c
+++ _printf.c
@@ -15,4 +15,13 @@ int _printf(const char *format, ...)
 		{"c", c_func}, {"s", s_func}, {"%", perc_func}
 		{NULL, NULL},
 	};
+	va_start(args, format);
+	i = 0, count = 0;
+	if (format == NULL) format[0] /*NULL?? */
+		return (1);
+	while (format != NULL && format[i] != '\0')
+		
+		j = 0;
+		while ()
+
 }
