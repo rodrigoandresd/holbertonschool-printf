99941cc21f6dd13659efcc30ab61298ffc77ed56
AvelinoC5 <avelino_63@hotmail.com>
2022-07-20 10:45:44 -0500

    update expansion character

--- _printf.c
+++ _printf.c
@@ -20,8 +20,10 @@ int _printf(const char *format, ...)
 	if (format == NULL) format[0] /*NULL?? */
 		return (1);
 	while (format != NULL && format[i] != '\0')
-		
+		{
+			if (format[i] != %)
+			_putchar(format[i]);
 		j = 0;
 		while ()
-
+		}
 }
