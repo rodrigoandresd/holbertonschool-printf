222979e61fc1e1805c5b6b72f11f835fa9a9420c
AvelinoC5 <avelino_63@hotmail.com>
2022-07-20 15:56:37 -0500

    functions

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * c_func - function that prints characters
+ * @args: character passed into function
+ * Return: counter
+ */
+
+int c_func(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return 0;
+}
