e3d0545dc943d0cc1b7fa963ee449c773b051d6f
AvelinoC5 <avelino_63@hotmail.com>
2022-07-20 10:31:04 -0500

    update struct

--- main.h
+++ main.h
@@ -5,8 +5,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-
 /* types and structures */
+
+/**
+ * struct vartype - struct vartype
+ * @vartype: type of variable to be printed
+ * @f: the function associated
+ */
+
+typedef struct vartype
+{
+	char *vartype;
+	int (*f)(va_list);
+} var_t;
+
 
 /* macros valores constantes */
 
