26b15eb58a9df3bccf5005ae0760b55180de3280
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-20 16:12:29 -0500

    continue functions

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 /**
  * c_func - function that prints characters
  * @args: character passed into function
- * Return: counter
+ * Return: success
  */
 
 int c_func(va_list args)
@@ -14,3 +14,23 @@ int c_func(va_list args)
 	_putchar(c);
 	return 0;
 }
+
+/**
+ * s_func - function that prints string
+ * @args: character passed into function
+ * Return: success
+ */
+
+int s_func(va_list args)
+{
+	int i;
+	char *s;
+
+	s = va_arg(args, char *);
+	for (i = 0 ; s[i] ; i++)
+		_putchar(s[i]);
+	return 0;
+}
+
+
+
