a46b45960127a920deaea1ef76152efdea257629
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-20 21:13:48 -0500

    null

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 	i = 0, count = 0;
-	if (format == NULL || format[0] == '\0'))
+	if (format == NULL || format[0] == '\0')
 		return (1);
 	while (format != NULL && format[i] != '\0')
 	{
