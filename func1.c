262c9df59119204608a22be813fbc8beefa1791c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-21 20:28:13 -0500

    creation func1 file

---
+++ func1.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * d_func - print a decimal (base 10) number
+ * @args: argument list
+ * Return: count
+ */
+int d_func(va_list args)
+
+
+
+/**
+ * i_func - print an integer in base 10
+ * @args: argument list
+ * Return: count
+ */
+int i_func(va_list args)
