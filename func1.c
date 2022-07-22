aedbf3724353afcd93ef5594d209d35733b2ace2
AvelinoC5 <avelino_63@hotmail.com>
2022-07-21 20:46:12 -0500

    description the function

--- func1.c
+++ func1.c
@@ -6,8 +6,38 @@
  * Return: count
  */
 int d_func(va_list args)
+{
+	int item;
+	int unit = 1;
+	int base = 10;
+	int count = 0;
+	int number;
 
+	item = va_arg(args, int);
 
+	if (item < 0)
+	{
+		_putchar('-');
+		number = item * -unit;
+		count++;
+	}
+		else
+		{
+			number = item;
+		}
+	while (number / unit >= base)
+	{
+		unit *= base;
+	}
+	while (unit != 0)
+	{
+		_putchar('0' + number / unit);
+		number %= unit;
+		unit /= base;
+		count++;
+	}
+	return (count);
+}
 
 /**
  * i_func - print an integer in base 10
@@ -15,3 +45,9 @@ int d_func(va_list args)
  * Return: count
  */
 int i_func(va_list args)
+{
+	int count = 0;
+
+	count = d_func(args);
+	return (count);
+}
